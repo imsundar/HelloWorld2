c6a873d0c7ed7f1558c9f9f97e520a8611872ff9
sundar <sundar.eee6593@gmail.com>
2022-02-03 20:37:13 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
   cout << "hello world" << endl;
   //Updating and making a pull request to main : 
   cout << "updating the file in my branch" << endl;
+
+  //updating from remote and will be pushed into repo using git : 
+  cout << "some update : " << endl; 
   
   return 0; 
 }
---
+++ third.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+
+int someotherThirdFunc()
+{
+    cout << "third func added from remote repo which is being pushed : " << endl; 
+    return 1;
+}
